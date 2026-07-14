6114d40b60f0a02d0ff9c0291caab84be7facb92
Callum Stott <callum@seadowg.com>
2026-07-14 16:44:55 +0100

    Add test for clearing text

--- androidApp/src/test/kotlin/io/music_assistant/client/ui/compose/search/SearchInputTest.kt
+++ androidApp/src/test/kotlin/io/music_assistant/client/ui/compose/search/SearchInputTest.kt
@@ -1,13 +1,20 @@
 package io.music_assistant.client.ui.compose.search
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsFocused
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.assertIsNotFocused
 import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.v2.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextReplacement
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.music_assistant.client.support.get
+import musicassistantclient.composeapp.generated.resources.Res
+import musicassistantclient.composeapp.generated.resources.common_clear
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -39,5 +46,18 @@ class SearchInputTest {
 
         restorationTester.emulateSavedInstanceStateRestore()
         composeTestRule.onNodeWithText("blah").assertIsNotFocused()
+    }
+
+    @Test
+    fun `clears text when clear pressed`() {
+        composeTestRule.setContent {
+            SearchInput(onSearch = {}, placeHolder = "Search query")
+        }
+
+        composeTestRule.onNodeWithText("Search query").performTextReplacement("something")
+        composeTestRule.onNodeWithContentDescription(Res.string.common_clear.get()).performClick()
+
+        composeTestRule.onNodeWithText("something").assertIsNotDisplayed()
+        composeTestRule.onNodeWithText("Search query").assertIsDisplayed()
     }
 }
